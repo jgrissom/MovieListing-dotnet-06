b31ba5d668cda804cb14496936fb681b67b6b6c3
jgrissom <jgrissom91@gmail.com>
2023-02-26 18:56:58 -0600

    [1.05] Make sure movies.csv file exists

--- Program.cs
+++ Program.cs
@@ -7,6 +7,15 @@ string path = Directory.GetCurrentDirectory() + "\\nlog.config";
 var logger = LogManager.LoadConfiguration(path).GetCurrentClassLogger();
 logger.Info("Program started");
 
-Console.WriteLine("Hello World!");
+string file = "movies.csv";
+// make sure movie file exists
+if (!File.Exists(file))
+{
+  logger.Error("File does not exist: {File}", file);
+}
+else
+{
+  // TODO: create user menu
+}
 
 logger.Info("Program ended");
